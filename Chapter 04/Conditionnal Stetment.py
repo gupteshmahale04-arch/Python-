d9035489f896879e6f3b58b84869dfbcbd913eeb
Guptesh Mahale <gupteshmahale04@gmail.com>
2026-09-17 18:09:54 +0530

    Add files via upload

---
+++ Chapter 04/Conditionnal Stetment.py
@@ -0,0 +1,17 @@
+# conditional stetment
+marks=int(input("Enter the markes"))
+
+if marks<=90:
+    print("your grade is A+")
+elif marks<=80:
+    print("your grade is A")
+elif marks<=70:
+    print("your grade is B+")
+elif marks<=60:
+    print("your grade is B")
+elif marks<=50:
+    print("your grade is c+")
+elif marks<=40:
+    print("your grade is c")
+else :
+    print("your are fail")
---
+++ Chapter 04/tempCodeRunnerFile.py
@@ -0,0 +1,7 @@
+num=int(input(" Enterr The Number ::"))
+num1=1
+while(num1<=num):
+    if ( num1%2==0):
+      print("Number is Even ",num1)
+    num1=num1+1
+   
